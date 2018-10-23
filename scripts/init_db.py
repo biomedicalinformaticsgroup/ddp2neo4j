f9e76054be2a3cf428b6a571bb74a19592a488e4
hxin <hxin87@gmail.com>
2018-10-23 16:37:58 +0100

    reformat

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,14 +1,12 @@
-from ddp2neo4j.repository.hpo_repo import HPOReposity
 from ddp2neo4j.core.parser import OboParser
-from ddp2neo4j.resources.res import HPO_Resource
-from ddp2neo4j.repository.patient_repo import PatientReposity
 from ddp2neo4j.core.parser import PatientParser
+from ddp2neo4j.repository.hpo_repo import HPOReposity
+from ddp2neo4j.repository.patient_repo import PatientReposity
+from ddp2neo4j.resources.res import HPO_Resource
 from ddp2neo4j.resources.res import Patient_Resource
 
-hpo_terms = OboParser.parse_file(HPO_Resource.file_dev,True)
-HPOReposity.batch_create_from_dict(hpo_terms,batch_size=3)
-
-
+hpo_terms = OboParser.parse_file(HPO_Resource.file_dev, True)
+HPOReposity.batch_create_from_dict(hpo_terms, batch_size=3)
 
 patient_records = PatientParser.parse_file(Patient_Resource.file_dev, True)
-PatientReposity.batch_create_from_dict(patient_records,batch_size=1)
+PatientReposity.batch_create_from_dict(patient_records, batch_size=1)
